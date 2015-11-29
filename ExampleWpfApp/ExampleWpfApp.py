b8d58ba62bfe4236a33469007f8e04de26cf843b
simone.deponti <simone.deponti@gmail.com>
2015-11-29 12:32:21 +0000

    Initial commit

---
+++ ExampleWpfApp/ExampleWpfApp.py
@@ -0,0 +1,18 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'ExampleWpfApp.xaml')
+        self.greetButton.Click += self.greet
+
+    def greet(self, sender, event):
+        name = self.nameTextBox.Text
+        greeting = "Hello {name}".format(name=name)
+        self.outputTextBlock.Text = greeting
+    
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
